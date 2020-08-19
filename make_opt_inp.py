53fabb9a44aadda092ff54889656280a39a4d807
EricBoittier <eric.boittier@icloud.com>
2020-08-19 14:24:44 +0200

    first commit

---
+++ make_opt_inp.py
@@ -0,0 +1,25 @@
+import os
+
+file_template = """%chk={0}.chk
+%mem={1}MB
+#p wB97XD/6-31G** opt freq=NoRaman dft-d3=bj
+
+0 1
+{2}
+\n
+"""
+
+
+dirs = [x for x in os.listdir(".") if os.path.isdir(x) and not x.__contains__(".")]
+
+for d in dirs:
+    unopt = open(os.path.join(d, "{}_unopt.xyz".format(d)), "r").readlines()
+    unopt = unopt[2:] #  remove header
+    s = ""
+    for x in unopt:
+        s += x
+    print(file_template.format(d+"_opt", 500, s))
+
+
+
+
